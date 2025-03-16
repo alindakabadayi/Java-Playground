c33824988d1913bfc58c64c7ec00a80559e5bd1f
Alinda Kabadayı <alindakabadayi@gmail.com>
2025-03-16 17:37:45 +0300

    Add KosulluDurumlar
    if/else if/else structures

--- 1-Basics/KosulluDurumlar/src/Main.java
+++ 1-Basics/KosulluDurumlar/src/Main.java
@@ -1,5 +1,31 @@
+// If/Else Blokları
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("If/Else Blokları");
+        System.out.println("\n=== If/Else Blokları ===");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Lütfen yaşınızı giriniz: ");
+
+        int yas = scanner.nextInt();
+
+        if(18 <= yas && yas < 65) {
+            System.out.println("Araç kullanabilirsiniz!");
+        }
+
+        else if(65 <= yas) {
+            System.out.println("Gözetim Dahilinde Araç Kullanabilirsiniz!");
+        }
+
+        else {
+            System.out.println("Araç Kullanamazsınız!");
+        }
+
+
     }
 }
+// else blokları tek başına yazılamaz. if değişkenine bağlı olarak kullanılırlar.
+// else if blokları, if bloğu çalışmazsa kontrol edilir.
