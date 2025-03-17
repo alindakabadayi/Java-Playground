58c541df03fd166d914e6047c3c1932726693874
Alinda Kabadayı <alindakabadayi@gmail.com>
2025-03-18 02:25:42 +0300

    Create Donguler Project
    Add Main.java (for loops)

---
+++ 1-Basics/Donguler/src/Main.java
@@ -0,0 +1,27 @@
+/**
+ * Döngüler
+ * @author alindakabadayi
+ * @version 0.1
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\n=== DÖNGÜLER ===\n");
+
+
+        // for(Başlatma; Koşul; Arttırma veya Azaltma İşlemleri) {}
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("i = " + i);
+        }
+
+
+
+/*
+        // i -= 2 -> i = i - 2
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.println("i = " + i);
+        }
+ */
+    }
+}
