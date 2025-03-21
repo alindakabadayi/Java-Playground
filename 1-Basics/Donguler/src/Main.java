78befb35b88ed57f991f8f36a8df064edc2cc623
Alinda Kabadayı <alindakabadayi@gmail.com>
2025-03-21 11:53:00 +0300

    Add for loops to Main.java

--- 1-Basics/Donguler/src/Main.java
+++ 1-Basics/Donguler/src/Main.java
@@ -9,19 +9,31 @@ public class Main {
 
         System.out.println("\n=== DÖNGÜLER ===\n");
 
+    // for(Başlatma; Koşul; Arttırma veya Azaltma İşlemleri) {}
 
-        // for(Başlatma; Koşul; Arttırma veya Azaltma İşlemleri) {}
+
         for (int i = 0; i <= 10; i++) {
             System.out.println("i = " + i);
         }
 
 
-
-/*
-        // i -= 2 -> i = i - 2
-        for (int i = 10; i >= 0; i -= 2) {
+        for (int i = 10; i >= 0; i -= 2) {    // i -= 2 -> i = i - 2
             System.out.println("i = " + i);
         }
- */
+        
+/*
+        int i = 0;
+        int j = 0;
+
+        for (; i < 10 && j > 0; i++, j--) {
+            System.out.println("i = " + i);
+            System.out.println("j = " + j);
+        }
+
+    // i < 10 ve j > 0 olduğu zaman ve i'yi 1 arttırmak sürece ve j'yi 1 azaltmak sürece döngü çalışır
+
+  */
+
     }
+
 }
