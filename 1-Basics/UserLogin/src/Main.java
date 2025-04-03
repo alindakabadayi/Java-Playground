1c900982aa1a282efff925dae759502ee61f7233
Alinda Kabadayı <alindakabadayi@gmail.com>
2025-04-03 23:30:56 +0300

    Create user login system with while loops
    Includes username/password check, limited to 3 attempts, and while-based loop control.

---
+++ 1-Basics/UserLogin/src/Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+/**
+ * Kullanıcı Girişi Programı
+ * @author alindakabadayi
+ * @version 0.1
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n=== Kullanıcı Girişi Programı ===\n");
+
+        Scanner input = new Scanner(System.in);
+
+        int giris_hakki = 3;
+
+
+        String sys_kullanici_adi = "Alinda Kabadayı";
+        String sys_kullanici_sifre = "1234";
+
+        System.out.println("Kullanıcı Girişine Hoşgeldiniz");
+        System.out.println("------------------");
+
+        while (true) {
+            System.out.println("Kullanıcı Adı: ");
+            String kullanici = input.nextLine();
+
+            System.out.println("Parola: ");
+            String parola = input.nextLine();
+
+            if (kullanici.equals(sys_kullanici_adi) && parola.equals(sys_kullanici_sifre)) {
+
+                System.out.println("Sisteme Giriş Başarılı");
+                break;
+            }
+            else if (kullanici.equals(sys_kullanici_adi) && !parola.equals(sys_kullanici_sifre)) {
+
+                System.out.println("Parolanız Yanlış !");
+
+                giris_hakki--;
+                System.out.println("Giriş Hakkı: " + giris_hakki);
+            }
+            else if (!kullanici.equals(sys_kullanici_adi)&& parola.equals(sys_kullanici_sifre)) {
+
+                System.out.println("Kullanıcı Adınız Yanlış !");
+
+                giris_hakki--;
+                System.out.println("Giriş Hakkı: " + giris_hakki);
+            }
+            else {
+                System.out.println("Kullanıcı Adınız ve Parolanız Yanlış !");
+
+                giris_hakki--;
+                System.out.println("Giriş Hakkı: " + giris_hakki);
+            }
+
+            if (giris_hakki == 0) {
+                System.out.println("Giriş Hakkınız Bitti ! Lütfen Daha Sonra Tekrar Deneyiniz ");
+                break;
+            }
+        }
+    }
+}
